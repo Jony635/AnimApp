b0b7ada118d21d9eeec94e0375c62e803887eeef
Jony635 <jony635molina@gmail.com>
2019-07-25 16:06:14 +0200

    search button in action bar with the onclick ready and nice icon

---
+++ AnimApp/app/src/main/java/com/jony635/animapp/MainActivity.java
@@ -0,0 +1,51 @@
+package com.jony635.animapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.mainmenu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item)
+    {
+        switch(item.getItemId())
+        {
+            case R.id.SearchButton:
+            {
+                OnSearchButtonPressed();
+                break;
+            }
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+        return false;
+    }
+
+    private void OnSearchButtonPressed()
+    {
+        //Open another activity, replace the action bar with a custom one, which:
+            // - Has to have a "Back" button.
+            // - Contains an "Input Text" bar.
+            // - Shows below the options matched. //CHOOSE ONE: Button or auto search?
+    }
+}
